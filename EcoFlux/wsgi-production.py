7b7bcf2c7c198e7cc64ded80bc6072499d5622ea
Hank Knox <hank.knox@gmail.com>
2025-08-08 20:12:45 -0400

    Add a modified wsgi.py file to use in a production environment.
    wsgi-production.py

---
+++ EcoFlux/wsgi-production.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for EcoFlux project in production.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EcoFlux.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
