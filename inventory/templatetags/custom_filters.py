750ca98d23f90db4abfbd39e22284e33b74bbd15
Hank Knox <hank.knox@gmail.com>
2026-02-28 15:36:13 -0500

    Fix logic error in custom_filters.generate_summary
    Need to find a closing bracket AFTER the opening bracket in order to
    avoid an endless loop.

--- inventory/templatetags/custom_filters.py
+++ inventory/templatetags/custom_filters.py
@@ -1,4 +1,8 @@
+import logging
+
 from django import template
+
+logger = logging.getLogger("inventory")
 
 register = template.Library()
 
@@ -8,7 +12,7 @@ def get_field_display(obj, field_name):
     """Custom filter to access dynamic attributes"""
     text = getattr(obj, field_name, "")
     if field_name == "display_summary":
-        # strip HTML tags from fieldnote.note if there is no .summary
+        # strip HTML from fieldnote.note when there is no .summary
         text = generate_summary(text)
     return text
 
@@ -26,15 +30,17 @@ def generate_summary(text, max_chars=200):
     text = text.split("<br>")[0]
     i = text.find("<")
     while True:
-        if i == -1:
+        if i < 0:
             # No more possible tags to excise
             break
-        j = text.find(">")
+        j = text.find(">", i)
         # HTML tags will typically not be longer than 9 chars
         if j - i <= 9:
             # Excise this tag
             text = text[:i] + text[j + 1 :]
             i = text.find("<")
         else:
+            # Log a warning if we find someting longer
+            logger.warning(f"Unexpectedly long tag: {text[i: j+ 1]}")
             i = text.find("<", i + 1)
     return text[:max_chars]
