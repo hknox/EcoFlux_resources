f8b00ae5004b1a52f41df3c8d5e1615b02ad37fe
Hank Knox <hank.knox@gmail.com>
2026-04-08 11:44:37 -0400

    Add django-maintenance-mode.
    This adds a simple way using .env MAINTENANCE_* variables to
    disable/enable the site.

---
+++ inventory/maintenance_testing.py
@@ -0,0 +1,7 @@
+"""This module is to play with new features while in maintenance mode."""
+
+
+def get_context_data(**kwargs):
+    context_d = {"new_feature": "- Uploading docs"}
+
+    return context_d
