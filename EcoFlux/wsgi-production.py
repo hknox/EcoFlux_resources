d024111926346c2df8e45361b94bb56b93c66e4c
Hank Knox <hank.knox@gmail.com>
2026-08-10 21:51:18 -0300

    Minor clode cleanup from Flymake suggestions.
    Remove unused imports in wsgi-production.py, inventory/urls.py.
    Move an import to top of file in wsgi-production.py.

--- EcoFlux/wsgi-production.py
+++ EcoFlux/wsgi-production.py
@@ -7,12 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
-
 from django.core.wsgi import get_wsgi_application
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
 
 application = get_wsgi_application()
